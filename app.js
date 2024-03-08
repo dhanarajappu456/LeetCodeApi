04788bb769e3acdcfaf544f9f63acba3431054be
Daan <dhanrajappu456@gamail.com>
2024-03-08 15:52:14 +0530

    app

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get("/userInfo/:user", (req, res) => {
       });
     })
     .catch((err) => {
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: "err.message" });
     });
 });
 
