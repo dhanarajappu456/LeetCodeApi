3691cbd8dc8d6ff47e3820045c03f17733c23755
Daan <dhanrajappu456@gamail.com>
2024-03-08 13:19:40 +0530

    update to app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 //const axios = require("./axios");
 const port = 3000;
 
