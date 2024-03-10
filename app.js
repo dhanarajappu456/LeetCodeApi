3ce9e9c4c794db232e894d0c40e2c68534431dc6
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-03-10 19:56:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = 3000;
 
 app.get("/test", (req, res) => res.send("Test working"));
 
+
 app.get("/userInfo/:user", async (req, res) => {
   const url = "https://leetcode.com/graphql";
 
@@ -32,99 +33,98 @@ app.get("/userInfo/:user", async (req, res) => {
     }
   }
 }`;
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Referer: "https://leetcode.com",
-      },
-      body: JSON.stringify({ query: query }),
-    });
-    const result = await response.json();
-    // result = {
-    //   data: {
-    //     allQuestionsCount: [
-    //       {
-    //         difficulty: "All",
-    //         count: 3073,
-    //       },
-    //       {
-    //         difficulty: "Easy",
-    //         count: 778,
-    //       },
-    //       {
-    //         difficulty: "Medium",
-    //         count: 1613,
-    //       },
-    //       {
-    //         difficulty: "Hard",
-    //         count: 682,
-    //       },
-    //     ],
-    //     matchedUser: {
-    //       problemsSolvedBeatsStats: [
-    //         {
-    //           difficulty: "Easy",
-    //           percentage: 98.71,
-    //         },
-    //         {
-    //           difficulty: "Medium",
-    //           percentage: 98.98,
-    //         },
-    //         {
-    //           difficulty: "Hard",
-    //           percentage: 98.16,
-    //         },
-    //       ],
-    //       submitStatsGlobal: {
-    //         acSubmissionNum: [
-    //           {
-    //             difficulty: "All",
-    //             count: 770,
-    //           },
-    //           {
-    //             difficulty: "Easy",
-    //             count: 228,
-    //           },
-    //           {
-    //             difficulty: "Medium",
-    //             count: 428,
-    //           },
-    //           {
-    //             difficulty: "Hard",
-    //             count: 114,
-    //           },
-    //         ],
-    //       },
-    //     },
-    //   },
-    // };
-    res.status(200).json({
-      data: result.data,
-    });
-  } catch (err) {
-    res.status(500).json({ message: "err.message" });
-  }
-  // const response = await fetch(url, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Referer: "https://leetcode.com",
-  //   },
-  //   body: JSON.stringify({ query: query }),
-  // })
-  // .then((result) => result.json())
-  // .then((result) => {
+  // try {
+  //   const response = await fetch(url, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Referer: "https://leetcode.com",
+  //     },
+  //     body: JSON.stringify({ query: query }),
+  //   });
+  //   const result = await response.json();
+  //   result = {
+  //     data: {
+  //       allQuestionsCount: [
+  //         {
+  //           difficulty: "All",
+  //           count: 3073,
+  //         },
+  //         {
+  //           difficulty: "Easy",
+  //           count: 778,
+  //         },
+  //         {
+  //           difficulty: "Medium",
+  //           count: 1613,
+  //         },
+  //         {
+  //           difficulty: "Hard",
+  //           count: 682,
+  //         },
+  //       ],
+  //       matchedUser: {
+  //         problemsSolvedBeatsStats: [
+  //           {
+  //             difficulty: "Easy",
+  //             percentage: 98.71,
+  //           },
+  //           {
+  //             difficulty: "Medium",
+  //             percentage: 98.98,
+  //           },
+  //           {
+  //             difficulty: "Hard",
+  //             percentage: 98.16,
+  //           },
+  //         ],
+  //         submitStatsGlobal: {
+  //           acSubmissionNum: [
+  //             {
+  //               difficulty: "All",
+  //               count: 770,
+  //             },
+  //             {
+  //               difficulty: "Easy",
+  //               count: 228,
+  //             },
+  //             {
+  //               difficulty: "Medium",
+  //               count: 428,
+  //             },
+  //             {
+  //               difficulty: "Hard",
+  //               count: 114,
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   };
   //   res.status(200).json({
   //     data: result.data,
   //   });
-  // })
-  // .catch((err) => {
+  // } catch (err) {
   //   res.status(500).json({ message: "err.message" });
-  // });
+  // }
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Referer: "https://leetcode.com",
+    },
+    body: JSON.stringify({ query: query }),
+  })
+    .then((result) => result.json())
+    .then((result) => {
+      res.status(200).json({
+        data: result.data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "err.message" });
+    });
 });
 
-// axios.post(url)
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
