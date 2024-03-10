938e4a29df6698d5b6d3aecea921b6211bf661f7
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-03-10 20:44:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -122,7 +122,9 @@ app.get("/userInfo/:user", async (req, res) => {
       });
     })
     .catch((err) => {
-      res.status(500).json({ message: err.message});
+      console.log(err)
+      res.status(500).json({ message: "err"});
+      
     });
 });
 
