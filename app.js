1370f5462fd8d8774eb393246884d3f53cc45512
Daan <dhanrajappu456@gamail.com>
2024-03-11 09:39:55 +0530

    chnage app

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+
 //const axios = require("./axios");
 const port = 3000;
-
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://blog-guxz.vercel.app");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.get("/test", (req, res) => res.send("Test working"));
 
 app.get("/userInfo/:user", async (req, res) => {
@@ -32,99 +42,132 @@ app.get("/userInfo/:user", async (req, res) => {
     }
   }
 }`;
-  try {
-    // const response = await fetch(url, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Referer: "https://leetcode.com",
-    //   },
-    //   body: JSON.stringify({ query: query }),
-    // });
-    // const result = await response.json();
-    result = {
-      data: {
-        allQuestionsCount: [
-          {
-            difficulty: "All",
-            count: 3073,
-          },
-          {
-            difficulty: "Easy",
-            count: 778,
-          },
-          {
-            difficulty: "Medium",
-            count: 1613,
-          },
-          {
-            difficulty: "Hard",
-            count: 682,
-          },
-        ],
-        matchedUser: {
-          problemsSolvedBeatsStats: [
-            {
-              difficulty: "Easy",
-              percentage: 98.71,
-            },
-            {
-              difficulty: "Medium",
-              percentage: 98.98,
-            },
-            {
-              difficulty: "Hard",
-              percentage: 98.16,
-            },
-          ],
-          submitStatsGlobal: {
-            acSubmissionNum: [
-              {
-                difficulty: "All",
-                count: 770,
-              },
-              {
-                difficulty: "Easy",
-                count: 228,
-              },
-              {
-                difficulty: "Medium",
-                count: 428,
-              },
-              {
-                difficulty: "Hard",
-                count: 114,
-              },
-            ],
-          },
-        },
-      },
-    };
-    res.status(200).json({
-      data: result.data,
-    });
-  } catch (err) {
-    res.status(500).json({ message: "err.message" });
-  }
-  // const response = await fetch(url, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Referer: "https://leetcode.com",
-  //   },
-  //   body: JSON.stringify({ query: query }),
-  // })
-  // .then((result) => result.json())
-  // .then((result) => {
+  // try {
+  //   const response = await fetch(url, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Referer: "https://leetcode.com",
+  //     },
+  //     body: JSON.stringify({ query: query }),
+  //   });
+  //   const result = await response.json();
+  //   result = {
+  //     data: {
+  //       allQuestionsCount: [
+  //         {
+  //           difficulty: "All",
+  //           count: 3073,
+  //         },
+  //         {
+  //           difficulty: "Easy",
+  //           count: 778,
+  //         },
+  //         {
+  //           difficulty: "Medium",
+  //           count: 1613,
+  //         },
+  //         {
+  //           difficulty: "Hard",
+  //           count: 682,
+  //         },
+  //       ],
+  //       matchedUser: {
+  //         problemsSolvedBeatsStats: [
+  //           {
+  //             difficulty: "Easy",
+  //             percentage: 98.71,
+  //           },
+  //           {
+  //             difficulty: "Medium",
+  //             percentage: 98.98,
+  //           },
+  //           {
+  //             difficulty: "Hard",
+  //             percentage: 98.16,
+  //           },
+  //         ],
+  //         submitStatsGlobal: {
+  //           acSubmissionNum: [
+  //             {
+  //               difficulty: "All",
+  //               count: 770,
+  //             },
+  //             {
+  //               difficulty: "Easy",
+  //               count: 228,
+  //             },
+  //             {
+  //               difficulty: "Medium",
+  //               count: 428,
+  //             },
+  //             {
+  //               difficulty: "Hard",
+  //               count: 114,
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   };
   //   res.status(200).json({
   //     data: result.data,
   //   });
-  // })
-  // .catch((err) => {
+  // } catch (err) {
   //   res.status(500).json({ message: "err.message" });
-  // });
+  // }
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Referer: "https://leetcode.com",
+    },
+    body: JSON.stringify({ query: query }),
+  })
+    .then((result) => {
+      console.log("one", result);
+
+      return result.json();
+    })
+    .then((result) => {
+      console.log("two", result);
+      res.status(200).json({
+        data: result.data,
+      });
+      console.log("four");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "err" });
+    });
 });
 
-// axios.post(url)
+app.get("/meal", async (req, res) => {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/categories.php",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((result) => {
+      console.log("one", result);
+
+      return result.json();
+    })
+    .then((result) => {
+      console.log("two", result);
+      res.status(200).json({
+        data: result,
+      });
+      console.log("four");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "err" });
+    });
+});
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
